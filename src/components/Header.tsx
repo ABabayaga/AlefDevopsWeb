bbd00fcecd7d85f9ce1991132efa50c5db9d4cec
ABabayaga <alef_rdl@hotmail.com>
2025-04-06 20:26:26 +0400

    Update add MonitorCrypto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,6 +39,7 @@ const Header: React.FC = () => {
               <NavDropdown.Item href="https://token100-alef-devops-front.vercel.app/" target="_blank">Token100</NavDropdown.Item>
               <NavDropdown.Item href="https://market-place-alef-devops-frontend-no-backend.vercel.app/" target="_blank">MarketPlace Blockchain</NavDropdown.Item>
               <NavDropdown.Item href="https://voting-frontend-xi.vercel.app/" target="_blank">Voting Blockchain</NavDropdown.Item>
+              <NavDropdown.Item href="https://monitor-crypto-frontend.vercel.app/" target="_blank">MonitorCrypto</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={Link} href="/blog">Blog</Nav.Link>
             <Nav.Link as={Link} href="/about">About Me</Nav.Link>
